301298991a7d6c7cc1fd8ff7fad5524af1f6f60d
dome4 <dominic.seitz@gmx.de>
2018-01-17 22:44:27 +0100

    read more issue fixed

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ add_filter('excerpt_length', 'minti_new_excerpt_length');
 function minti_custom_excerpt($limit=50) {
     global $minti_data;
     if($minti_data['switch_readmore'] != 0) {
-        return strip_shortcodes(wp_trim_words(get_the_content(), $limit, '... <a class="read-more-link" href="'. get_permalink() .'">' . __('read more', 'minti') . '  &rarr;</a>'));
+        return strip_shortcodes(get_the_content('read more →'));
     } else {
         return strip_shortcodes(wp_trim_words(get_the_content(), $limit));
     }
