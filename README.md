b4784a8fdd897fbdc91a385dab702a1f0f365d01
dome4 <dominic.seitz@gmx.de>
2018-01-17 22:43:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unicon Theme #
+
+Modified version of the following wordpress theme:
+[http://unicon.hellominti.com/](http://unicon.hellominti.com/)
+
+Due to the license of the theme, only the modified snippet of the theme was uploaded.
---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * other code
+ */
+
+/* ------------------------------------------------------------------------ */
+/* Custom Excerpts
+/* ------------------------------------------------------------------------ */
+// Set new Default Excerpt Length
+function minti_new_excerpt_length($length) {
+    return 200;
+}
+add_filter('excerpt_length', 'minti_new_excerpt_length');
+
+// Custom Excerpt Length
+function minti_custom_excerpt($limit=50) {
+    global $minti_data;
+    if($minti_data['switch_readmore'] != 0) {
+        return strip_shortcodes(wp_trim_words(get_the_content(), $limit, '... <a class="read-more-link" href="'. get_permalink() .'">' . __('read more', 'minti') . '  &rarr;</a>'));
+    } else {
+        return strip_shortcodes(wp_trim_words(get_the_content(), $limit));
+    }
+}
+
+/*
+ * other code
+ */
